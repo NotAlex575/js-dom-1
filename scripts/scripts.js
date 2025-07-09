c8c998bbb169cfa4fc4edf59d0ca1054cea85b72
NotAlex575 <dapontealessandro89gmail.com>
2025-07-09 18:46:18 +0200

    extra mio: aggiunto sottotitolo

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,7 @@
 const accendi_bottone = document.getElementById('accendi-bottone');
 const accendi_lampadina = document.getElementById('accendi-immagine');
-const testo_bianco = document.getElementById('light-text');
+const testo_bianco_titolo = document.getElementById('light-text-title');
+const testo_bianco_sottotitolo = document.getElementById('light-text-subtitle');
 const schermo_bianco = document.getElementById('bg-black');
 const testo_accendi_spegni = document.getElementById('accendi-spegni-testo')
 
@@ -9,9 +10,11 @@ accendi_bottone.addEventListener('click', function(){
         //accendi la lampadina
         accendi_lampadina.classList.add('spegni-lampadina');
         accendi_lampadina.classList.remove('accendi-lampadina');
-        //cambio il color del testo da bianco a nero
-        testo_bianco.classList.add('color-black');
-        testo_bianco.classList.remove('color-white');
+        //cambio il color del testo del titolo e sottotitolo da bianco a nero
+        testo_bianco_titolo.classList.add('color-black');
+        testo_bianco_titolo.classList.remove('color-white');
+        testo_bianco_sottotitolo.classList.add('color-black');
+        testo_bianco_sottotitolo.classList.remove('color-white');
         //cambio il background del sito da nero a bianco
         schermo_bianco.classList.add('background-white');
         schermo_bianco.classList.remove('background-black');
@@ -22,9 +25,11 @@ accendi_bottone.addEventListener('click', function(){
         //spegni la lampadina
         accendi_lampadina.classList.add('accendi-lampadina');
         accendi_lampadina.classList.remove('spegni-lampadina');
-        //cambio il color del testo da nero a bianco
-        testo_bianco.classList.add('color-white');
-        testo_bianco.classList.remove('color-black');
+        //cambio il color del testo del titolo e sottotitolo da nero a bianco
+        testo_bianco_titolo.classList.add('color-white');
+        testo_bianco_titolo.classList.remove('color-black');
+        testo_bianco_sottotitolo.classList.add('color-white');
+        testo_bianco_sottotitolo.classList.remove('color-black');
         //cambio il background del sito da nero a bianco
         schermo_bianco.classList.add('background-black');
         schermo_bianco.classList.remove('background-white');
